d45db546680158c3efac84c8fd1de791dc60399c
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-06 09:36:54 +0000

    Setting circleci up

--- test/selenium.js
+++ test/selenium.js
@@ -3,11 +3,21 @@ const driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.c
 
 driver.get("file:///" + __dirname + "/index.html");
 driver.wait(webdriver.until.titleMatches(/: Complete$/), 5000).then(() => {
+    driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
+        entries.forEach((entry) => {
+            console.log('[%s] %s', entry.level.name, entry.message);
+        });
+    });
     driver.takeScreenshot().then((data) => {
         require("fs").writeFileSync("screenshot.png", data, "base64");
         driver.quit();
     });
 }).catch(() => {
+    driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
+        entries.forEach((entry) => {
+            console.log('[%s] %s', entry.level.name, entry.message);
+        });
+    });
     driver.takeScreenshot().then((data) => {
         require("fs").writeFileSync("screenshot.png", data, "base64");
         driver.quit();
@@ -15,8 +25,3 @@ driver.wait(webdriver.until.titleMatches(/: Complete$/), 5000).then(() => {
     driver.quit();
 });
 
-driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
-    entries.forEach((entry) => {
-        console.log('[%s] %s', entry.level.name, entry.message);
-    });
-});
