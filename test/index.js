b1acaa3a0cf14d681c5cd4b3cc274915d65ce0e4
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-06 07:42:55 +0000

    Setting circleci up

--- test/index.js
+++ test/index.js
@@ -3,28 +3,20 @@ const driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.c
 
 console.log("Loading...");
 driver.get("file:///" + __dirname + "/index.html");
-driver.takeScreenshot().then((data) => {
-    require("fs").writeFileSync("screenshot.png", data, "base64");
-});
 console.log("Wait for test results...");
-driver.findElement(webdriver.By.id("mocha-stats"));
-const result = driver.executeScript(`
-    const passedTests = document.querySelectorAll(".test.pass");
-    const failedTests = document.querySelectorAll(".test.fail");
-    return {
-        passed: passedTests.length
-        failed: failedTests.length
-    };
-`);
-
-console.log("Test result :");
-console.log(result.passed + " test(s) passed.");
-console.log(result.failed + " test(s) failed.");
-
-console.log("Tear down...");
-driver.quit();
-
-if (result.failed) {
-    process.exit(1);
-}
-//process.exit(0);
+driver.findElement(webdriver.By.id("mocha-stats")).then(() => {
+    driver.takeScreenshot().then((data) => {
+        require("fs").writeFileSync("screenshot.png", data, "base64");
+    });
+    const result = driver.executeScript(`
+        const passedTests = document.querySelectorAll(".test.pass");
+        const failedTests = document.querySelectorAll(".test.fail");
+        return {
+            passed: passedTests.length
+            failed: failedTests.length
+        };
+    `);
+    console.log(result.passed + " test(s) passed.");
+    console.log(result.failed + " test(s) failed.");
+    driver.quit();
+});
