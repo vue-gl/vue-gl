277c065e15322f18d4e88c79c8eff67ce5cb75db
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-06 10:03:52 +0000

    Setting circleci up

--- test/selenium.js
+++ test/selenium.js
@@ -1,30 +1,20 @@
 const webdriver = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const options = new chrome.Options();
-options.addArguments("--allow-file-access-from-files");
+options.addArguments("--disable-web-security");
 const driver = new webdriver.Builder().withCapabilities(options.toCapabilities()).build();
 
 driver.get("file:///" + __dirname + "/index.html");
-driver.wait(webdriver.until.titleMatches(/: Complete$/), 5000).then(() => {
-    driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
-        entries.forEach((entry) => {
-            console.log('[%s] %s', entry.level.name, entry.message);
-        });
-    });
-    driver.takeScreenshot().then((data) => {
-        require("fs").writeFileSync("screenshot.png", data, "base64");
-        driver.quit();
-    });
-}).catch(() => {
-    driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
-        entries.forEach((entry) => {
-            console.log('[%s] %s', entry.level.name, entry.message);
-        });
-    });
-    driver.takeScreenshot().then((data) => {
-        require("fs").writeFileSync("screenshot.png", data, "base64");
-        driver.quit();
-    });
-    driver.quit();
-});
+driver.wait(webdriver.until.titleMatches(/: Complete$/), 5000).then(exit).catch(exit);
 
+function exit() {
+    driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
+        entries.forEach((entry) => {
+            console.log('[%s] %s', entry.level.name, entry.message);
+        });
+    });
+    driver.takeScreenshot().then((data) => {
+        require("fs").writeFileSync("screenshot.png", data, "base64");
+        driver.quit();
+    });
+}
