ad82e282ec8ccb1ddd40d95ecae350d532d98c3e
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-06 15:07:34 +0000

    Setting circleci up.

--- .circleci/selenium.js
+++ .circleci/selenium.js
@@ -1,8 +1,8 @@
 const webdriver = require("selenium-webdriver");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
 
-driver.get("http://localhost:8080/test/index.html");
-driver.wait(webdriver.until.titleMatches(/: Complete$/), 5000).then(exit).catch(exit);
+driver.get("http://localhost:8080/test/");
+driver.wait(webdriver.until.titleMatches(/: Complete$/), 20).then(exit).catch(exit);
 
 function exit() {
     driver.manage().logs().get(webdriver.logging.Type.BROWSER).then((entries) => {
@@ -10,6 +10,7 @@ function exit() {
             console.log('[%s] %s', entry.level.name, entry.message);
         });
     });
+    require("fs").writeFileSync(driver.executeScript("result.html", "return document.documentElement.outerHTML"));
     driver.takeScreenshot().then((data) => {
         require("fs").writeFileSync("screenshot.png", data, "base64");
         driver.quit();
