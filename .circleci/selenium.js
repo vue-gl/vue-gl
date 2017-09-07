5b7d6f25e68e8b48312f4a31b39e5024361adbb4
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-07 01:17:09 +0000

    Setting circleci up.

--- .circleci/selenium.js
+++ .circleci/selenium.js
@@ -1,16 +1,28 @@
+const fs = require("fs");
 const webdriver = require("selenium-webdriver");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
 
 driver.get("http://localhost:8080/test/");
 driver.wait(webdriver.until.titleMatches(/: Complete$/), 20).then(() => {
-    driver.executeScript(`return document.querySelectorAll(".test.fail")`).then((elms) => {
+    driver.executeScript(`return document.querySelectorAll("#mocha-report")`).then((rt) => {
         driver.executeScript("return document.documentElement.outerHTML").then((html) => {
-            require("fs").writeFileSync("test-result.html", html);
-            driver.quit();
-            process.exit(elms.length);
+            fs.writeFile("test-result.html", html, (err) => {
+                if (err) throw err;
+                const tests = rt.querySelectorAll(".test");
+                const passedTests = rt.querySelectorAll(".test.pass:not(.pending)");
+                const failedTests = rt.querySelectorAll(".test.fail");
+                const pendingTests = rt.querySelectorAll(".test.pending)");
+                if (!tests.length) throw "No tests are evaluated.";
+                console.log(`${passedTests.length} of ${tests.length} test${passedTests.length === 1 ? " is": "s are"} passed.`);
+                console.log(`${failedTests.length} of ${tests.length} test${failedTests.length === 1 ? " is": "s are"} failed.`);
+                console.log(`${pendingTests.length} of ${tests.length} test${pendingTests.length === 1 ? " is": "s are"} pended.`);
+                driver.quit();
+                process.exit(failedTests.length ? 1: 0);
+            });
         });
     });
 }).catch((err) => {
     console.error(err);
+    driver.quit();
     process.exit(1);
 });
