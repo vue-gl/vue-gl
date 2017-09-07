1838ef7edc36949bae961c1e7de0ad79bd57e888
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-07 01:35:28 +0000

    Setting circleci up.

--- .circleci/selenium.js
+++ .circleci/selenium.js
@@ -4,21 +4,19 @@ const driver = new webdriver.Builder().forBrowser("chrome").build();
 
 driver.get("http://localhost:8080/test/");
 driver.wait(webdriver.until.titleMatches(/: Complete$/), 20).then(() => {
-    driver.executeScript(`return document.querySelector("#mocha-report")`).then((rt) => {
-        driver.executeScript("return document.documentElement.outerHTML").then((html) => {
-            fs.writeFile("test-result.html", html, (err) => {
-                if (err) throw err;
-                const tests = rt.querySelectorAll(".test");
-                const passedTests = rt.querySelectorAll(".test.pass:not(.pending)");
-                const failedTests = rt.querySelectorAll(".test.fail");
-                const pendingTests = rt.querySelectorAll(".test.pending)");
-                if (!tests.length) throw "No tests are evaluated.";
-                console.log(`${passedTests.length} of ${tests.length} test${passedTests.length === 1 ? " is": "s are"} passed.`);
-                console.log(`${failedTests.length} of ${tests.length} test${failedTests.length === 1 ? " is": "s are"} failed.`);
-                console.log(`${pendingTests.length} of ${tests.length} test${pendingTests.length === 1 ? " is": "s are"} pended.`);
-                driver.quit();
-                process.exit(failedTests.length ? 1: 0);
-            });
+    driver.executeScript("return document.documentElement.outerHTML").then((html) => {
+        fs.writeFile("test-result.html", html, (err) => {
+            if (err) throw err;
+            const tests = driver.findElements(webdriver.By.cssSelector(".test"));
+            const passedTests = driver.findElements(webdriver.By.cssSelector(".test.pass:not(.pending)"));
+            const failedTests = driver.findElements(webdriver.By.cssSelector(".test.fail"));
+            const pendingTests = driver.findElements(webdriver.By.cssSelector(".test.pending)"));
+            if (!tests.length) throw "No tests are evaluated.";
+            console.log(`${passedTests.length} of ${tests.length} test${passedTests.length === 1 ? " is": "s are"} passed.`);
+            console.log(`${failedTests.length} of ${tests.length} test${failedTests.length === 1 ? " is": "s are"} failed.`);
+            console.log(`${pendingTests.length} of ${tests.length} test${pendingTests.length === 1 ? " is": "s are"} pended.`);
+            driver.quit();
+            process.exit(failedTests.length ? 1: 0);
         });
     });
 }).catch((err) => {
