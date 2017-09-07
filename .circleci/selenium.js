2cce643322a7c53ff14d1b2f361d70c43385abf9
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-07 01:45:50 +0000

    Setting circleci up.

--- .circleci/selenium.js
+++ .circleci/selenium.js
@@ -7,10 +7,10 @@ driver.wait(webdriver.until.titleMatches(/: Complete$/), 10000).then(() => {
     driver.executeScript("return document.documentElement.outerHTML").then((html) => {
         fs.writeFile("test-result.html", html, (err) => {
             if (err) throw err;
-            const tests = driver.findElements(webdriver.By.cssSelector(".test"));
-            const passedTests = driver.findElements(webdriver.By.cssSelector(".test.pass:not(.pending)"));
-            const failedTests = driver.findElements(webdriver.By.cssSelector(".test.fail"));
-            const pendingTests = driver.findElements(webdriver.By.cssSelector(".test.pending)"));
+            const tests = driver.findElements(webdriver.By.css(".test"));
+            const passedTests = driver.findElements(webdriver.By.css(".test.pass:not(.pending)"));
+            const failedTests = driver.findElements(webdriver.By.css(".test.fail"));
+            const pendingTests = driver.findElements(webdriver.By.css(".test.pending)"));
             if (!tests.length) throw "No tests are evaluated.";
             console.log(`${passedTests.length} of ${tests.length} test${passedTests.length === 1 ? " is": "s are"} passed.`);
             console.log(`${failedTests.length} of ${tests.length} test${failedTests.length === 1 ? " is": "s are"} failed.`);
