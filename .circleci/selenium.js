bb5e03bd784a64df1283505c861987e282cec8b1
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-07 01:53:11 +0000

    Setting circleci up.

--- .circleci/selenium.js
+++ .circleci/selenium.js
@@ -7,16 +7,21 @@ driver.wait(webdriver.until.titleMatches(/: Complete$/), 10000).then(() => {
     driver.executeScript("return document.documentElement.outerHTML").then((html) => {
         fs.writeFile("test-result.html", html, (err) => {
             if (err) throw err;
-            const tests = driver.findElements(webdriver.By.css(".test"));
-            const passedTests = driver.findElements(webdriver.By.css(".test.pass:not(.pending)"));
-            const failedTests = driver.findElements(webdriver.By.css(".test.fail"));
-            const pendingTests = driver.findElements(webdriver.By.css(".test.pending)"));
-            if (!tests.length) throw "No tests are evaluated.";
-            console.log(`${passedTests.length} of ${tests.length} test${passedTests.length === 1 ? " is": "s are"} passed.`);
-            console.log(`${failedTests.length} of ${tests.length} test${failedTests.length === 1 ? " is": "s are"} failed.`);
-            console.log(`${pendingTests.length} of ${tests.length} test${pendingTests.length === 1 ? " is": "s are"} pended.`);
-            driver.quit();
-            process.exit(failedTests.length ? 1: 0);
+            driver.findElements(webdriver.By.css(".test")).then((tests) => {
+                driver.findElements(webdriver.By.css(".test.pass:not(.pending)")).then((passedTests) => {
+                    driver.findElements(webdriver.By.css(".test.fail")).then((failedTests) => {
+                        driver.findElements(webdriver.By.css(".test.pending)")).then((pendingTests) => {
+                            if (!tests.length) throw "No tests are evaluated.";
+                            console.log(`${passedTests.length} of ${tests.length} test${passedTests.length === 1 ? " is": "s are"} passed.`);
+                            console.log(`${failedTests.length} of ${tests.length} test${failedTests.length === 1 ? " is": "s are"} failed.`);
+                            console.log(`${pendingTests.length} of ${tests.length} test${pendingTests.length === 1 ? " is": "s are"} pended.`);
+                            const exitCode = failedTests.length ? 1: 0;
+                            driver.quit();
+                            process.exit(exitCode);
+                        });
+                    });
+                });
+            });
         });
     });
 }).catch((err) => {
