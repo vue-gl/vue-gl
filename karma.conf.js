51a5b22fa084be4c9ab99300067abec1918ff543
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-23 13:52:31 +0000

    Add the testing browsers.

--- karma.conf.js
+++ karma.conf.js
@@ -114,6 +114,84 @@ module.exports = (config) => {
                 platform: "Windows 10",
                 browserName: "chrome",
                 version: "latest"
+            },
+            "Firefox 4 on Windows 10": {
+                base: "SauceLabs",
+                platform: "Windows 10",
+                browserName: "firefox",
+                version: "4"
+            },
+            "Firefox (latest) on Windows 10": {
+                base: "SauceLabs",
+                platform: "Windows 10",
+                browserName: "firefox",
+                version: "latest"
+            },
+            "Internet Explorer (latest) on Windows 10": {
+                base: "SauceLabs",
+                platform: "Windows 10",
+                browserName: "internet explorer",
+                version: "latest"
+            },
+            "Internet Explorer (latest) on Windows 10": {
+                base: "SauceLabs",
+                platform: "Windows 10",
+                browserName: "internet explorer",
+                version: "latest"
+            },
+            "Safari (latest) on Mac OS X Sierra": {
+                base: "SauceLabs",
+                platform: "macOS 10.12",
+                browserName: "safari",
+                version: "latest"
+            },
+            "Chrome (latest) on Mac OS X Sierra": {
+                base: "SauceLabs",
+                platform: "macOS 10.12",
+                browserName: "chrome",
+                version: "latest"
+            },
+            "Chrome 27 on Mac OS X Sierra": {
+                base: "SauceLabs",
+                platform: "macOS 10.12",
+                browserName: "chrome",
+                version: "27"
+            },
+            "Firefox (latest) on Mac OS X Sierra": {
+                base: "SauceLabs",
+                platform: "macOS 10.12",
+                browserName: "chrome",
+                version: "latest"
+            },
+            "Firefox 4 on Mac OS X Sierra": {
+                base: "SauceLabs",
+                platform: "macOS 10.12",
+                browserName: "chrome",
+                version: "4"
+            },
+            "Safari 9 on Mac OS X El Capitan": {
+                base: "SauceLabs",
+                platform: "OS X 10.11",
+                browserName: "safari",
+                version: "9"
+            },
+            "Safari 8 on Mac OS X Yosemite": {
+                base: "SauceLabs",
+                platform: "OS X 10.10",
+                browserName: "safari",
+                version: "8"
+            },
+            "Safari 7 on Mac OS X Mavericks": {
+                base: "SauceLabs",
+                platform: "OS X 10.9",
+                browserName: "safari",
+                version: "7"
+            },
+            "Safari 6 on Mac OS X Mountain Lion": {
+                base: "SauceLabs",
+                platform: "OS X 10.8",
+                browserName: "safari",
+                version: "6"
             }
         };
         options.browsers = Object.keys(options.customLaunchers);
