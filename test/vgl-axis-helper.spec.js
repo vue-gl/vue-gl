38c73197b4c343abc048b7422728dd1fe8fd27e6
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-09-23 04:08:11 +0000

    Use the slice method than from, to convert the array.

--- test/vgl-axis-helper.spec.js
+++ test/vgl-axis-helper.spec.js
@@ -7,7 +7,7 @@ describe("VglAxisHelperのテスト", function() {
             const vm = new Vue(VglAxisHelper);
             assert.isTrue(vm.inst.isLineSegments);
             assert.sameOrderedMembers(
-                Array.from(vm.inst.geometry.attributes.position.array),
+                Array.prototype.slice.call(vm.inst.geometry.attributes.position.array),
                 [
 		            0, 0, 0, 1, 0, 0,
 		            0, 0, 0, 0, 1, 0,
