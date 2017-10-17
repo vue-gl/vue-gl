6cd334457bd8fad90c63d7919e15d968a74e41da
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-10-17 12:23:02 +0000

    Fix the error when webgl is not supported.

--- test/vgl-renderer.spec.js
+++ test/vgl-renderer.spec.js
@@ -1,7 +1,7 @@
 describe("VglRenderer component", function() {
     const {VglRenderer, VglNamespace} = VueGL;
     const assert = chai.assert;
-    const webgl = (() => {/*
+    before(function() {
         const canvas = document.createElement("canvas");
         const opts = {
             antialias: true,
@@ -9,24 +9,21 @@ describe("VglRenderer component", function() {
             depth: false
         };
         try {
-            return canvas.getContext("webgl", opts) || canvas.getContext("experimental-webgl", opts);
-        } catch(e) {}*/
-        return false;
-    })();
-    let pvm;
-    if (!webgl) {
-        console.log("WebGL is not supported. Skip tests for the VglRenderer.");
-    } else {
-        //pvm = new Vue(VglRenderer).$mount();
-    }
+            this.webgl = canvas.getContext("webgl", opts) || canvas.getContext("experimental-webgl", opts);
+        } catch(e) {}
+        if (this.webgl) {
+            this.vm = new Vue(VglRenderer).$mount();
+        } else {
+            console.log("WebGL is not supported. Skip tests for the VglRenderer.");
+        }
+    });
     beforeEach(function() {
-        // Skip all tests when the WebGL is not supported.
-        /*if (!webgl)*/ this.skip();
+        if (!this.webgl) this.skip();
     });
     describe("Namespace injection", function() {
         describe("Should be able to access vglCameras", function() {
             it("When the component is the root namespace.", function() {
-                assert.isObject(pvm.vglCameras);
+                assert.isObject(this.vm.vglCameras);
             });
             it("When the component is a descendant of the other namespace.", function() {
                 const vm = new Vue({
@@ -41,7 +38,7 @@ describe("VglRenderer component", function() {
         });
         describe("Should be able to access vglScenes", function() {
             it("When the component is the root namespace.", function() {
-                assert.isObject(pvm.vglScenes);
+                assert.isObject(this.vm.vglScenes);
             });
             it("When the component is a descendant of the other namespace.", function() {
                 const vm = new Vue({
@@ -58,13 +55,13 @@ describe("VglRenderer component", function() {
     describe("Creating a renderer", function() {
         describe("Output canvas", function() {
             it("The domElement property of WebGLRenderer instance should be the Vue created canvas.", function() {
-                assert.strictEqual(pvm.$refs.rdr, pvm.inst.domElement);
+                assert.strictEqual(this.vm.$refs.rdr, this.vm.inst.domElement);
             });
         });
         describe("Context attributes", function() {
             describe("The alpha property should affect the alpha attribute.", function() {
                 it("When the property is undefined (or false).", function() {
-                    assert.isFalse(pvm.inst.getContextAttributes().alpha);
+                    assert.isFalse(this.vm.inst.getContextAttributes().alpha);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {alpha: true}}).$mount();
@@ -72,13 +69,17 @@ describe("VglRenderer component", function() {
                 });
             });
             describe("The disableDepth property should affect the depth attribute.", function() {
-                const enable = !webgl.getContextAttributes().depth;
-                if (!enable) console.log("Depth buffer cannot be disabled. Skip tests for the depth property.");
+                before(function() {
+                    if (this.webgl) {
+                        this.depth = !this.webgl.getContextAttributes().depth;
+                        if (!this.depth) console.log("Depth buffer cannot be disabled. Skip tests for the depth property.");
+                    }
+                });
                 beforeEach(function() {
-                    if (!enable) this.skip();
+                    if (!this.depth) this.skip();
                 });
                 it("When the property is undefined (or false).", function() {
-                    assert.isTrue(pvm.inst.getContextAttributes().depth);
+                    assert.isTrue(this.vm.inst.getContextAttributes().depth);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {disableDepth: true}}).$mount();
@@ -87,7 +88,7 @@ describe("VglRenderer component", function() {
             });
             describe("The disableStencil property should affect the stencil attribute.", function() {
                 it("When the property is undefined (or false).", function() {
-                    assert.isTrue(pvm.inst.getContextAttributes().stencil);
+                    assert.isTrue(this.vm.inst.getContextAttributes().stencil);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {disableStencil: true}}).$mount();
@@ -95,13 +96,17 @@ describe("VglRenderer component", function() {
                 });
             });
             describe("The antialias property should affect the antialias attribute.", function() {
-                const enable = webgl.getContextAttributes().antialias;
-                if (!enable) console.log("Antialiasing is not supported. Skip tests for the antialias property.");
+                before(function() {
+                    if (this.webgl) {
+                        this.antialias = this.webgl.getContextAttributes().antialias;
+                        if (!this.antialias) console.log("Antialiasing is not supported. Skip tests for the antialias property.");
+                    }
+                });
                 beforeEach(function() {
-                    if (!enable) this.skip();
+                    if (!this.antialias) this.skip();
                 });
                 it("When the property is undefined (or false).", function() {
-                    assert.isFalse(pvm.inst.getContextAttributes().antialias);
+                    assert.isFalse(this.vm.inst.getContextAttributes().antialias);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {antialias: true}}).$mount();
@@ -109,13 +114,17 @@ describe("VglRenderer component", function() {
                 });
             });
             describe("The disablePremultipliedAlpha property should affect the premultipliedAlpha attribute.", function() {
-                const enable = !webgl.getContextAttributes().premultipliedAlpha;
-                if (!enable) console.log("PremultipliedAlpha cannot be disabled. Skip tests for the premultipliedAlpha property.");
+                before(function() {
+                    if (this.webgl) {
+                        this.premultipliedAlpha = !this.webgl.getContextAttributes().premultipliedAlpha;
+                        if (!this.premultipliedAlpha) console.log("PremultipliedAlpha cannot be disabled. Skip tests for the premultipliedAlpha property.");
+                    }
+                });
                 beforeEach(function() {
-                    if (!enable) this.skip();
+                    if (!this.premultipliedAlpha) this.skip();
                 });
                 it("When the property is undefined (or false).", function() {
-                    assert.isTrue(pvm.inst.getContextAttributes().premultipliedAlpha);
+                    assert.isTrue(this.vm.inst.getContextAttributes().premultipliedAlpha);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {disablePremultipliedAlpha: true}}).$mount();
@@ -124,7 +133,7 @@ describe("VglRenderer component", function() {
             });
             describe("The preserveDrawingBuffer property should affect the preserveDrawingBuffer attribute.", function() {
                 it("When the property is undefined (or false).", function() {
-                    assert.isFalse(pvm.inst.getContextAttributes().preserveDrawingBuffer);
+                    assert.isFalse(this.vm.inst.getContextAttributes().preserveDrawingBuffer);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {preserveDrawingBuffer: true}}).$mount();
@@ -134,13 +143,17 @@ describe("VglRenderer component", function() {
         });
         describe("Capabilities", function() {
             describe("The logarithmicDepthBuffer property should affect the logarithmicDepthBuffer capability.", function() {
-                const ext = webgl.getSupportedExtensions().indexOf("EXT_frag_depth") >= 0;
-                if (!ext) console.log("The extension EXT_frag_depth is not supported. Skip tests for the logarithmicDepthBuffer property.");
+                before(function() {
+                    if (this.webgl) {
+                        this.logarithmicDepthBuffer = this.webgl.getSupportedExtensions().indexOf("EXT_frag_depth") >= 0;
+                        if (!this.logarithmicDepthBuffer) console.log("The extension EXT_frag_depth is not supported. Skip tests for the logarithmicDepthBuffer property.");
+                    }
+                });
                 beforeEach(function() {
-                    if (!ext) this.skip();
+                    if (!this.logarithmicDepthBuffer) this.skip();
                 });
                 it("When the property is undefined (or false).", function() {
-                    assert.isFalse(pvm.inst.capabilities.logarithmicDepthBuffer);
+                    assert.isFalse(this.vm.inst.capabilities.logarithmicDepthBuffer);
                 });
                 it("When the property is true.", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {logarithmicDepthBuffer: true}}).$mount();
@@ -149,7 +162,7 @@ describe("VglRenderer component", function() {
             });
             describe("The precision property should affect the precision capability.", function() {
                 it("When the property is undefined (or false).", function() {
-                    assert.strictEqual(pvm.inst.capabilities.precision, "highp");
+                    assert.strictEqual(this.vm.inst.capabilities.precision, "highp");
                 });
                 it("When the property is \"lowp\".", function() {
                     const vm = new (Vue.extend(VglRenderer))({propsData: {precision: "lowp"}}).$mount();
