3f5a5d961e63f0ee0287b57ce0f3e1ae4b75d5a0
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2018-02-04 13:28:22 +0000

    refactor: Make easy-to-read.

--- src/vgl-cylinder-geometry.js
+++ src/vgl-cylinder-geometry.js
@@ -1,27 +1,31 @@
 import VglGeometry from './vgl-geometry.js';
 import { CylinderGeometry } from './three.js';
-import { parseFloatEx, parseIntEx, createObjectFromArray } from './utils.js';
-
-const validator = [String, Number];
-
-const props = [
-  'radiusTop',
-  'radiusBottom',
-  'height',
-  'radialSegments',
-  'heightSegments',
-  'openEnded',
-  'thetaStart',
-  'thetaLength',
-];
+import { validatePropNumber } from './utils.js';
 
 export default {
   mixins: [VglGeometry],
-  props: createObjectFromArray(props, (key, i) => (i === 5 ? Boolean : validator)),
+  props: {
+    radiusTop: { type: validatePropNumber, default: 1 },
+    radiusBottom: { type: validatePropNumber, default: 1 },
+    height: { type: validatePropNumber, default: 1 },
+    radialSegments: { type: validatePropNumber, default: 8 },
+    heightSegments: { type: validatePropNumber, default: 1 },
+    openEnded: Boolean,
+    thetaStart: { type: validatePropNumber, default: 0 },
+    thetaLength: { type: validatePropNumber, default: Math.PI * 2 },
+  },
   computed: {
     inst() {
-      const opts = props.map((key, i) => (i < 3 || i > 5 ? parseFloatEx : parseIntEx)(this[key]));
-      return new CylinderGeometry(...opts);
+      return new CylinderGeometry(
+        parseFloat(this.radiusTop),
+        parseFloat(this.radiusBottom),
+        parseFloat(this.height),
+        parseInt(this.radialSegments, 10),
+        parseInt(this.heightSegments, 10),
+        this.openEnded,
+        parseFloat(this.thetaStart),
+        parseFloat(this.thetaLength),
+      );
     },
   },
 };
