2e94be2a41d52a8ad877d745f0710a481e1f1978
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2018-02-11 15:17:40 +0000

    test: tests for the scene component.

--- test/vgl-scene.spec.js
+++ test/vgl-scene.spec.js
@@ -1,80 +1,68 @@
-describe('VglScene component', function component() {
-  const { VglScene, VglRenderer } = VueGL;
-  const { assert } = chai;
-  before(function hook(done) {
-    this.WebGLRenderer = THREE.WebGLRenderer;
-    THREE.WebGLRenderer = function WebGLRenderer() {
-      this.shadowMap = {};
-    };
-    THREE.WebGLRenderer.prototype.setSize = () => {};
-    done();
-  });
-  after(function hook(done) {
-    THREE.WebGLRenderer = this.WebGLRenderer;
-    done();
-  });
-  describe('The instance should be registered to the injected namespace.', function suite() {
-    it('Should be registered when created.', function test(done) {
-      const vm = new Vue({
-        template: '<vgl-renderer ref="mr"><vgl-scene name="dm\'&^>" ref="me" /><other-component ref="other" /></vgl-renderer>',
-        components: {
-          VglScene,
-          VglRenderer,
-          OtherComponent: {
-            inject: ['vglScenes'],
-            render() {},
-          },
-        },
-      }).$mount();
-      assert.strictEqual(vm.$refs.other.vglScenes.forGet["dm'&^>"], vm.$refs.me.inst.uuid);
-      done();
+describe('VglScene:', function suite() {
+  const { VglScene, VglNamespace } = VueGL;
+  const { expect } = chai;
+  it('without properties', function test(done) {
+    const vm = new Vue({
+      template: '<vgl-namespace><vgl-scene ref="s" /></vgl-namespace>',
+      components: { VglScene, VglNamespace },
+    }).$mount();
+    vm.$nextTick(() => {
+      try {
+        const actual = vm.$refs.s.inst.clone();
+        actual.updateMatrixWorld();
+        const expected = new THREE.Scene();
+        expected.updateMatrixWorld();
+        expected.uuid = actual.uuid;
+        expect(actual.toJSON()).to.deep.equal(expected.toJSON());
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
-    it('Should be unregistered when destroyed.', function test(done) {
-      const vm = new Vue({
-        template: '<vgl-renderer ref="mr"><vgl-scene name="n<!--" v-if="!destroyed" /><other-component ref="other" /></vgl-renderer>',
-        components: {
-          VglScene,
-          VglRenderer,
-          OtherComponent: {
-            inject: ['vglScenes'],
-            render() {},
-          },
-        },
-        data: { destroyed: false },
-      }).$mount();
-      assert.hasAllKeys(vm.$refs.other.vglScenes.forGet, ['n<!--']);
-      vm.destroyed = true;
+  });
+  it('with properties', function test(done) {
+    const vm = new Vue({
+      template: '<vgl-namespace><vgl-scene position="8 3 -3.5" rotation="0.8 0.8 0.5 XZY" scale="1.3 1.4 1.1" ref="s" /></vgl-namespace>',
+      components: { VglScene, VglNamespace },
+    }).$mount();
+    vm.$nextTick(() => {
+      try {
+        const actual = vm.$refs.s.inst.clone();
+        actual.updateMatrixWorld();
+        const expected = new THREE.Scene();
+        expected.position.set(8, 3, -3.5);
+        expected.rotation.set(0.8, 0.8, 0.5, 'XZY');
+        expected.scale.set(1.3, 1.4, 1.1);
+        expected.updateMatrixWorld();
+        expected.uuid = actual.uuid;
+        expect(actual.toJSON()).to.deep.equal(expected.toJSON());
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+  it('after properties are changed', function test(done) {
+    const vm = new Vue({
+      template: '<vgl-namespace><vgl-scene :position="p" :rotation="r" :scale="s" ref="s" /></vgl-namespace>',
+      components: { VglScene, VglNamespace },
+      data: { p: '0 1 0', r: '0 0 0.2 XYZ', s: '1.1 0.9 0.8' },
+    }).$mount();
+    vm.$nextTick(() => {
+      vm.p = '1.1 2 0.8';
+      vm.r = '0.23 0.4 1.1 YZX';
+      vm.s = '0.8 0.7 0.9';
       vm.$nextTick(() => {
         try {
-          assert.isEmpty(vm.$refs.other.vglScenes.forGet);
-          done();
-        } catch (e) {
-          done(e);
-        }
-      });
-    });
-    it('Should be replaced when the instance is replaced.', function test(done) {
-      const vm = new Vue({
-        template: '<vgl-renderer><mixed-in name="\'<!--" ref="geo" /><other-component ref="other" /></vgl-renderer>',
-        components: {
-          VglRenderer,
-          MixedIn: {
-            mixins: [VglScene],
-            computed: {
-              inst() { return this.i; },
-            },
-            data: () => ({ i: new THREE.Scene() }),
-          },
-          OtherComponent: {
-            inject: ['vglScenes'],
-            render() {},
-          },
-        },
-      }).$mount();
-      vm.$refs.geo.i = new THREE.Scene();
-      vm.$nextTick(() => {
-        try {
-          assert.strictEqual(vm.$refs.other.vglScenes.forGet["'<!--"], vm.$refs.geo.inst.uuid);
+          const actual = vm.$refs.s.inst.clone();
+          actual.updateMatrixWorld();
+          const expected = new THREE.Scene();
+          expected.position.set(1.1, 2, 0.8);
+          expected.rotation.set(0.23, 0.4, 1.1, 'YZX');
+          expected.scale.set(0.8, 0.7, 0.9);
+          expected.updateMatrixWorld();
+          expected.uuid = actual.uuid;
+          expect(actual.toJSON()).to.deep.equal(expected.toJSON());
           done();
         } catch (e) {
           done(e);
