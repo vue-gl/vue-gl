e4acfd50d37dc72f1391bf5f018de3ca224d37ca
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2018-03-05 14:19:43 +0000

    add: new mesh depth material component.

---
+++ src/materials/vgl-mesh-depth-material.js
@@ -0,0 +1,31 @@
+import { VglMaterialWithMap } from '../mixins.js';
+import { MeshDepthMaterial } from '../three.js';
+import { string, boolean } from '../validators.js';
+
+/**
+ * A material for drawing geometry by depth,
+ * corresponding [THREE.MeshDepthMaterial](https://threejs.org/docs/index.html#api/materials/MeshDepthMaterial).
+ * This material is not affected by lights.
+ *
+ * Properties of [VglMaterial](vgl-material) are also available as mixin.
+ */
+
+export default {
+  mixins: [VglMaterialWithMap],
+  props: {
+    /** Whether the material is affected by fog. */
+    fog: boolean,
+    /** The color map of the material. */
+    map: string,
+  },
+  computed: {
+    inst: () => new MeshDepthMaterial(),
+  },
+  watch: {
+    inst: {
+      handler(inst) { Object.assign(inst, { fog: this.fog }); },
+      immediate: true,
+    },
+    fog(fog) { this.inst.fog = fog; },
+  },
+};
