8ad00b3920730e3d4a1384d73f2f16c7a93847b5
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2018-03-07 02:02:00 +0000

    feat: color and normal attributes of geometry component.

--- src/core/vgl-geometry.js
+++ src/core/vgl-geometry.js
@@ -15,6 +15,10 @@ export default {
     name: string,
     /** The x, y, and z coordinates of each vertex in this geometry. */
     positionAttribute: floatArray,
+    /** The red, green, and blue channels of vertex color of each vertex in this geometry. */
+    colorAttribute: floatArray,
+    /** The x, y, and z components of the vertex normal vector of each vertex in this geometry. */
+    normalAttribute: floatArray,
   },
   computed: {
     inst: () => new BufferGeometry(),
@@ -27,6 +31,18 @@ export default {
             oldInst.getAttribute('position') :
             new BufferAttribute(new Float32Array(parseArray(this.positionAttribute)), 3);
           inst.addAttribute('position', positionAttribute);
+        }
+        if (this.colorAttribute) {
+          const colorAttribute = oldInst ?
+            oldInst.getAttribute('color') :
+            new BufferAttribute(new Float32Array(parseArray(this.colorAttribute)), 3);
+          inst.addAttribute('color', colorAttribute);
+        }
+        if (this.normalAttribute) {
+          const normalAttribute = oldInst ?
+            oldInst.getAttribute('normal') :
+            new BufferAttribute(new Float32Array(parseArray(this.normalAttribute)), 3);
+          inst.addAttribute('normal', normalAttribute);
         }
         if (oldInst) oldInst.dispose();
         this.vglNamespace.geometries[this.name] = inst;
@@ -45,6 +61,26 @@ export default {
         attributeObject.copyArray(positionArray);
       } else {
         attributeObject.setArray(new Float32Array(positionArray));
+      }
+      attributeObject.needsUpdate = true;
+    },
+    colorAttribute(colorAttribute) {
+      const colorArray = parseArray(colorAttribute);
+      const attributeObject = this.inst.getAttribute('color');
+      if (attributeObject.array.length === colorArray.length) {
+        attributeObject.copyArray(colorArray);
+      } else {
+        attributeObject.setArray(new Float32Array(colorArray));
+      }
+      attributeObject.needsUpdate = true;
+    },
+    normalAttribute(normalAttribute) {
+      const normalArray = parseArray(normalAttribute);
+      const attributeObject = this.inst.getAttribute('normal');
+      if (attributeObject.array.length === normalArray.length) {
+        attributeObject.copyArray(normalArray);
+      } else {
+        attributeObject.setArray(new Float32Array(normalArray));
       }
       attributeObject.needsUpdate = true;
     },
--- test/vgl-geometry.spec.js
+++ test/vgl-geometry.spec.js
@@ -18,13 +18,15 @@ describe('VglGeometry:', function suite() {
   });
   it('with properties', function test(done) {
     const vm = new Vue({
-      template: '<vgl-namespace><vgl-geometry position-attribute="3, 1, 2, 2, -5, 6.3" ref="g" /></vgl-namespace>',
+      template: '<vgl-namespace><vgl-geometry position-attribute="3, 1, 2, 2, -5, 6.3" color-attribute="0.8, 0.7, 0.9, 1, 0.1, 0.2" normal-attribute="2, 0, 0, -3, 4, 3" ref="g" /></vgl-namespace>',
       components: { VglGeometry, VglNamespace },
     }).$mount();
     vm.$nextTick(() => {
       try {
         const expected = new THREE.BufferGeometry();
         expected.addAttribute('position', new THREE.BufferAttribute(new Float32Array([3, 1, 2, 2, -5, 6.3]), 3));
+        expected.addAttribute('color', new THREE.BufferAttribute(new Float32Array([0.8, 0.7, 0.9, 1, 0.1, 0.2]), 3));
+        expected.addAttribute('normal', new THREE.BufferAttribute(new Float32Array([2, 0, 0, -3, 4, 3]), 3));
         expect(expected.toJSON()).to.deep.equal(expected.copy(vm.$refs.g.inst).toJSON());
         done();
       } catch (e) {
@@ -34,18 +36,24 @@ describe('VglGeometry:', function suite() {
   });
   it('after properties are changed', function test(done) {
     const vm = new Vue({
-      template: '<vgl-namespace><vgl-geometry :position-attribute="positionAttribute" ref="g" /></vgl-namespace>',
+      template: '<vgl-namespace><vgl-geometry :position-attribute="positionAttribute" :color-attribute="colorAttribute" :normal-attribute="normalAttribute" ref="g" /></vgl-namespace>',
       components: { VglGeometry, VglNamespace },
       data: {
         positionAttribute: '3, 1, 2, 2, -5, 6.3',
+        colorAttribute: '0.8, 0.7, 0.9, 1, 0.1, 0.2',
+        normalAttribute: '2, 0, 0, -3, 4, 3',
       },
     }).$mount();
     vm.$nextTick(() => {
       vm.positionAttribute = '3.21, -1, 2.3, 2.08, -5, 6.3';
+      vm.colorAttribute = '0.85, 0.7, 0.9, 0.99, 0.11, 0.25';
+      vm.normalAttribute = '2.2, 0, -10, 3, 4.8, 3';
       vm.$nextTick(() => {
         try {
           const expected = new THREE.BufferGeometry();
           expected.addAttribute('position', new THREE.BufferAttribute(new Float32Array([3.21, -1, 2.3, 2.08, -5, 6.3]), 3));
+          expected.addAttribute('color', new THREE.BufferAttribute(new Float32Array([0.85, 0.7, 0.9, 0.99, 0.11, 0.25]), 3));
+          expected.addAttribute('normal', new THREE.BufferAttribute(new Float32Array([2.2, 0, -10, 3, 4.8, 3]), 3));
           expect(expected.toJSON()).to.deep.equal(expected.copy(vm.$refs.g.inst).toJSON());
           done();
         } catch (e) {
@@ -56,18 +64,24 @@ describe('VglGeometry:', function suite() {
   });
   it('after attribute lengths are extended', function test(done) {
     const vm = new Vue({
-      template: '<vgl-namespace><vgl-geometry :position-attribute="positionAttribute" ref="g" /></vgl-namespace>',
+      template: '<vgl-namespace><vgl-geometry :position-attribute="positionAttribute" :color-attribute="colorAttribute" :normal-attribute="normalAttribute" ref="g" /></vgl-namespace>',
       components: { VglGeometry, VglNamespace },
       data: {
         positionAttribute: '3, 1, 2, 2, -5, 6.3',
+        colorAttribute: '0.8, 0.7, 0.9, 1, 0.1, 0.2',
+        normalAttribute: '2.2, 0, -10, 3, 4.8, 3',
       },
     }).$mount();
     vm.$nextTick(() => {
       vm.positionAttribute = '3.21, -1, 2.3, 2.08, -5, 6.3, 2.4, 3.1, 1.1';
+      vm.colorAttribute = '0.85, 0.7, 0.9, 0.99, 0.11, 0.25, 0.1, 0.99, 0.5';
+      vm.normalAttribute = '-2.2, 0, -10, 3, 4.8, 3, 4, 4, 4';
       vm.$nextTick(() => {
         try {
           const expected = new THREE.BufferGeometry();
           expected.addAttribute('position', new THREE.BufferAttribute(new Float32Array([3.21, -1, 2.3, 2.08, -5, 6.3, 2.4, 3.1, 1.1]), 3));
+          expected.addAttribute('color', new THREE.BufferAttribute(new Float32Array([0.85, 0.7, 0.9, 0.99, 0.11, 0.25, 0.1, 0.99, 0.5]), 3));
+          expected.addAttribute('normal', new THREE.BufferAttribute(new Float32Array([-2.2, 0, -10, 3, 4.8, 3, 4, 4, 4]), 3));
           expect(expected.toJSON()).to.deep.equal(expected.copy(vm.$refs.g.inst).toJSON());
           done();
         } catch (e) {
@@ -78,18 +92,24 @@ describe('VglGeometry:', function suite() {
   });
   it('after attribute lengths are shortened', function test(done) {
     const vm = new Vue({
-      template: '<vgl-namespace><vgl-geometry :position-attribute="positionAttribute" ref="g" /></vgl-namespace>',
+      template: '<vgl-namespace><vgl-geometry :position-attribute="positionAttribute" :color-attribute="colorAttribute" :normal-attribute="normalAttribute" ref="g" /></vgl-namespace>',
       components: { VglGeometry, VglNamespace },
       data: {
         positionAttribute: '3, 1, 2, 2, -5, 6.3',
+        colorAttribute: '0.8, 0.7, 0.9, 1, 0.1, 0.2',
+        normalAttribute: '2.2, 0, -10, 3, 4.8, 3',
       },
     }).$mount();
     vm.$nextTick(() => {
       vm.positionAttribute = '3.21, -1, 2.3';
+      vm.colorAttribute = '0.85, 0.7, 0.95';
+      vm.normalAttribute = '-2, -1.1, 5';
       vm.$nextTick(() => {
         try {
           const expected = new THREE.BufferGeometry();
           expected.addAttribute('position', new THREE.BufferAttribute(new Float32Array([3.21, -1, 2.3]), 3));
+          expected.addAttribute('color', new THREE.BufferAttribute(new Float32Array([0.85, 0.7, 0.95]), 3));
+          expected.addAttribute('normal', new THREE.BufferAttribute(new Float32Array([-2, -1.1, 5]), 3));
           expect(expected.toJSON()).to.deep.equal(expected.copy(vm.$refs.g.inst).toJSON());
           done();
         } catch (e) {
