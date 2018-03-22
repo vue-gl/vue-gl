cc4f5c91c003bf1348060591a4106a9430e93e04
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2018-03-22 06:30:56 +0000

    test: add a test case of changing the light to be shown.

--- test/vgl-spot-light-helper.spec.js
+++ test/vgl-spot-light-helper.spec.js
@@ -100,4 +100,43 @@ describe('VglSpotLightHelper:', function suite() {
       });
     });
   });
+  it('after the light property is changed', function test(done) {
+    const vm = new Vue({
+      template: '<vgl-namespace><vgl-spot-light position="3.5 1 0.25" color="#f2e3a2" angle="1.11" distance="22.32" target="-210 -58 -30" name="l1" /><vgl-spot-light position="3.8 2 0.5" color="#e2f3b4" angle="1.1" distance="22.3" target="-21 -5.8 -3" name="l2" /><vgl-spot-light-helper :light="l" ref="o" /></vgl-namespace>',
+      components: { VglNamespace, VglSpotLight, VglSpotLightHelper },
+      data: { l: 'l1' },
+    }).$mount();
+    vm.$nextTick(() => {
+      vm.l = 'l2';
+      vm.$nextTick(() => {
+        // watchers called.
+        vm.$refs.o.vglNamespace.update();
+        vm.$nextTick(() => {
+          try {
+            const actual = new THREE.Object3D().copy(vm.$refs.o.inst.children[0]);
+            actual.updateMatrixWorld();
+            const light = new THREE.SpotLight(0xe2f3b4, undefined, 22.3, 1.1);
+            light.position.set(3.8, 2, 0.5);
+            const helper = new THREE.SpotLightHelper(light);
+            const expected = new THREE.Object3D().copy(helper);
+            expected.updateMatrixWorld();
+            actual.traverse((obj) => {
+              Object.assign(obj, { uuid: '' });
+              if (obj.geometry) Object.assign(obj.geometry, { uuid: '' });
+              if (obj.material) Object.assign(obj.material, { uuid: '' });
+            });
+            expected.traverse((obj) => {
+              Object.assign(obj, { uuid: '' });
+              if (obj.geometry) Object.assign(obj.geometry, { uuid: '' });
+              if (obj.material) Object.assign(obj.material, { uuid: '' });
+            });
+            expect(actual.toJSON()).to.deep.equal(expected.toJSON());
+            done();
+          } catch (e) {
+            done(e);
+          }
+        });
+      });
+    });
+  });
 });
