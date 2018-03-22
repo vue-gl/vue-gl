856e9f07115e5b8c68aa54abd03b47010e81ee01
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2018-03-22 06:44:01 +0000

    test: add a test case of that the light is not replaced.

--- test/vgl-directional-light-helper.spec.js
+++ test/vgl-directional-light-helper.spec.js
@@ -61,7 +61,7 @@ describe('VglDirectionalLightHelper:', function suite() {
       }
     });
   });
-  it('after properties are changed', function test(done) {
+  it('after properties (including the size property) are changed', function test(done) {
     const vm = new Vue({
       template: '<vgl-namespace><vgl-directional-light position="3.8 2 0.5" color="#e2f3b4" name="l" /><vgl-directional-light-helper light="l" :color="c" :size="s" ref="o" /></vgl-namespace>',
       components: { VglNamespace, VglDirectionalLight, VglDirectionalLightHelper },
@@ -70,6 +70,45 @@ describe('VglDirectionalLightHelper:', function suite() {
     vm.$nextTick(() => {
       vm.c = '#8e8e25';
       vm.s = 8.7;
+      vm.$nextTick(() => {
+        // watchers called.
+        vm.$refs.o.vglNamespace.update();
+        vm.$nextTick(() => {
+          try {
+            const actual = new THREE.Object3D().copy(vm.$refs.o.inst.children[0]);
+            actual.updateMatrixWorld();
+            const light = new THREE.DirectionalLight(0xe2f3b4);
+            light.position.set(3.8, 2, 0.5);
+            const helper = new THREE.DirectionalLightHelper(light, 8.7, 0x8e8e25);
+            const expected = new THREE.Object3D().copy(helper);
+            expected.updateMatrixWorld();
+            actual.traverse((obj) => {
+              Object.assign(obj, { uuid: '' });
+              if (obj.geometry) Object.assign(obj.geometry, { uuid: '' });
+              if (obj.material) Object.assign(obj.material, { uuid: '' });
+            });
+            expected.traverse((obj) => {
+              Object.assign(obj, { uuid: '' });
+              if (obj.geometry) Object.assign(obj.geometry, { uuid: '' });
+              if (obj.material) Object.assign(obj.material, { uuid: '' });
+            });
+            expect(actual.toJSON()).to.deep.equal(expected.toJSON());
+            done();
+          } catch (e) {
+            done(e);
+          }
+        });
+      });
+    });
+  });
+  it('after properties (except the size property) are changed', function test(done) {
+    const vm = new Vue({
+      template: '<vgl-namespace><vgl-directional-light position="3.8 2 0.5" color="#e2f3b4" name="l" /><vgl-directional-light-helper light="l" :color="c" size="8.7" ref="o" /></vgl-namespace>',
+      components: { VglNamespace, VglDirectionalLight, VglDirectionalLightHelper },
+      data: { c: '#dd678e' },
+    }).$mount();
+    vm.$nextTick(() => {
+      vm.c = '#8e8e25';
       vm.$nextTick(() => {
         // watchers called.
         vm.$refs.o.vglNamespace.update();
