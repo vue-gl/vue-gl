666067227a41baa486d4634e3c9c5f338de0544a
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2019-12-12 09:54:54 +0900

    doc: changes the jekyll theme

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,24 +1,31 @@
 import path from 'path';
 import copy from 'rollup-plugin-copy';
+import generateComponentDocuments from './rollup-plugins/rollup-plugin-generate-component-documents';
 import configArray from '../rollup.config';
 
 const [config] = configArray;
 
 Object.assign(config.output, {
-  file: path.resolve('docs/js/vue-gl.js'),
+  file: path.resolve(__dirname, 'js/vue-gl.js'),
 });
 
-config.plugins.push(copy({
-  targets: [{
-    src: require.resolve('vue/dist/vue.min'),
-    dest: path.resolve('docs/js'),
-  }, {
-    src: require.resolve('three/build/three.min'),
-    dest: path.resolve('docs/js'),
-  }, {
-    src: require.resolve('three/examples/fonts/helvetiker_regular.typeface.json'),
-    dest: path.resolve('docs/js'),
-  }],
-}));
+config.plugins.push(
+  copy({
+    targets: [{
+      src: require.resolve('vue/dist/vue.min'),
+      dest: path.resolve(__dirname, 'js'),
+    }, {
+      src: require.resolve('three/build/three.min'),
+      dest: path.resolve(__dirname, 'js'),
+    }, {
+      src: require.resolve('three/examples/fonts/helvetiker_regular.typeface.json'),
+      dest: path.resolve(__dirname, 'js'),
+    }],
+  }),
+  generateComponentDocuments({
+    src: path.resolve(__dirname, '../src'),
+    dest: path.resolve(__dirname, 'components'),
+  }),
+);
 
 export default config;
