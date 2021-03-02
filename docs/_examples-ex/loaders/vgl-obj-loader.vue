15f8f4fac2aa5a126da892c29e7902558cf82b00
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2021-03-02 15:51:23 +0900

    deps: compatible to three 0.126.0 (#1164)

--- docs/_examples-ex/loaders/vgl-obj-loader.vue
+++ docs/_examples-ex/loaders/vgl-obj-loader.vue
@@ -1,6 +1,9 @@
 <template>
-  <vgl-renderer>
-    <vgl-scene>
+  <vgl-renderer
+    camera="camera"
+    scene="scene"
+  >
+    <vgl-scene name="scene">
       <vgl-obj-loader src="/obj/seahorse.obj" />
       <vgl-ambient-light intensity="0.5" />
       <vgl-directional-light
@@ -9,6 +12,7 @@
       />
     </vgl-scene>
     <vgl-perspective-camera
+      name="camera"
       orbit-position="300 1 0.2"
       orbit-target="0 0 -300"
     />
